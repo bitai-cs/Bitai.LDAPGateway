1af0eaf135c897e60d6ae05c7a0ca1b9ac7db34a
Viko Bastidas <vico.bastidas@gmail.com>
2026-07-20 16:52:34 -0400

    Add NSwag & basic configuration.

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -8,7 +8,11 @@ builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApiDocument(config => {
+    config.DocumentName = "v1";
+    config.Title = "BITAI LDAP Gateway API";
+    config.Version = "v1";
+});
 
 var app = builder.Build();
 
@@ -16,8 +20,8 @@ app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+	app.UseOpenApi();
+	app.UseSwaggerUi();
 }
 
 app.UseHttpsRedirection();
